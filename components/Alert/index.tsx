eb64f265720db895f8e1f89249344d8b054d5444
sunyard_wangyang <995928555@qq.com>
2017-11-27 16:16:57 +0800

    test

---
+++ components/Alert/index.tsx
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import {Alert,Button} from 'antd';
+
+class Alert2 extends Component{
+
+  render(){
+    return(
+        <Button>button by dean</Button>
+    )
+  }
+
+}
+
+export default Alert2;
--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,3 @@
 export { default as Button } from './Button/index.tsx';
 
-export { default as Alert } from './Alert/index.jsx';
+export { default as Alert2 } from './Alert/index.tsx';
