840acd6085de691f52cd9b924ffd26d65dce052a
sunyard_wangyang <995928555@qq.com>
2017-11-27 15:23:15 +0800

    tesst

---
+++ components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class Button extends Component{
+  render(){
+    return(
+      <p>this is button</p>
+    )
+  }
+
+}
+
+export default new Button().render();
---
+++ components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Button } from './Button/index.tsx';
+
+export { default as Alert } from './Alert/index.jsx';
