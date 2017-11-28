32655dd6c3eba3abe52e19264e3cfdc4a24c4263
sunyard_wangyang <995928555@qq.com>
2017-11-28 11:33:28 +0800

    dd

---
+++ components/alert/index.jsx
@@ -0,0 +1,16 @@
+
+import React,{Component} from "react";
+import {Alert,Button} from 'antd';
+
+class Button22 extends Component{
+
+sdfsdf
+    render(){
+        return(
+            <Button>button by dean!!!~~~3333@@@@</Button>
+        )
+    }
+
+}
+
+export default Button22;
---
+++ components/index.jsx
@@ -0,0 +1,1 @@
+export { default as Button22 } from './Button/index.jsx';
